a1f9f0efc00dba1f10b832c25fc7977192c0f5be
Justin Schwartzenberger <schwarty@gmail.com>
2017-10-19 00:44:38 -0700

    add styling

---
+++ app/site-header.component.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular
+  .module('tuskDeskApp')
+  .component('siteHeader', {
+    template: '<header class="site-header"><h1>TuskDesk Ticket System</h1><nav><a href="#!/tickets">Tickets</a><a href="#!/companies">Companies</a></nav></header>',
+    controller: function () {}
+  });
--- app/tickets/ticket-overview/ticket-overview.component.js
+++ app/tickets/ticket-overview/ticket-overview.component.js
@@ -7,17 +7,16 @@ angular
     controller: ['userService', function (userService) {
         this.currentUserName = userService.currentUser();
         this._companies = [
-          {id: 1, title: 'Need help with UI', description: '', submittedBy: 'Mary', company: 'ABC Corp'},
-          {id: 2, title: 'Submit not working', description: '', submittedBy: 'Ned', company: 'Priority Gems'},
-          {id: 3, title: 'How do I upload files?', description: '', submittedBy: 'Bob', company: 'ABC Corp'},
+          {id: 1, title: 'Need help with UI', submittedBy: 'Mary', company: 'ABC Corp'},
+          {id: 2, title: 'Submit not working', submittedBy: 'Ned', company: 'Priority Gems'},
+          {id: 3, title: 'How do I upload files?', submittedBy: 'Bob', company: 'ABC Corp'},
           {
             id: 4,
             title: 'Schedule a video call for training',
-            description: '',
             submittedBy: 'Mary',
             company: 'ABC Corp'
           },
-          {id: 5, title: 'Minor issue with layout', description: '', submittedBy: 'Jules', company: 'Priority Gems'}
+          {id: 5, title: 'Minor issue with layout', submittedBy: 'Jules', company: 'Priority Gems'}
         ];
         this.tickets = this._companies;
 
